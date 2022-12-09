35aa026dbf2b319119c80843fef77eb95242345a
Jose Antonio Govea Millan <teckblendxd@gmail.com>
2022-12-09 12:07:17 -0600

    consultas credenciales

--- src/controllers/ControllerUsuario.ts
+++ src/controllers/ControllerUsuario.ts
@@ -135,6 +135,58 @@ export class ControllerUsuario {
       });
     }
   };
+
+  getCredencialesEmpleados = async (req: Request, res: Response) => {
+    try {
+  let querySQL = `SELECT * FROM loginEmpleado;`;
+  let respuesta: any = await execute.query(querySQL);
+  if (respuesta.validacion) {
+    res.send({
+      code: HttpCodes.aceptacion,
+      description: descriptions.aceptacion,
+      data: respuesta.data,
+    });
+  } else {
+    res.send({
+      code: HttpCodes.error,
+      description: respuesta.descripcion,
+    });
+  }
+} catch (e: any) {
+  res.send({
+    code: HttpCodes.error,
+    description: e.message,
+    data: null,
+  });
+}
+};
+  getCredencialesCliente = async (req: Request, res: Response) => {
+    try {
+  let querySQL = `SELECT * FROM loginCliente;`;
+  let respuesta: any = await execute.query(querySQL);
+  if (respuesta.validacion) {
+    res.send({
+      code: HttpCodes.aceptacion,
+      description: descriptions.aceptacion,
+      data: respuesta.data,
+    });
+  } else {
+    res.send({
+      code: HttpCodes.error,
+      description: respuesta.descripcion,
+    });
+  }
+} catch (e: any) {
+  res.send({
+    code: HttpCodes.error,
+    description: e.message,
+    data: null,
+  });
+}
+};
+
+
+
 }
 
 export default ControllerUsuario;
