4480aaae6607161b05b7ee056e20ae6267cb5708
Jose Antonio Govea Millan <teckblendxd@gmail.com>
2022-12-09 04:06:14 -0600

    updateCliente v1

--- dist/src/controllers/ControllerClientes.js
+++ dist/src/controllers/ControllerClientes.js
@@ -132,6 +132,68 @@ class ControllerClientes {
                 });
             }
         });
+        this.updateCliente = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield transaction.startTransaction();
+                let { 
+                //?cliente
+                idPersona, idUsuario, 
+                //?persona
+                nombre, apellidoPaterno, apellidoMaterno, calle, colonia, numero, codigo_postal, fechaNacimiento, genero, telefono, 
+                //?usuarioLogin
+                usuario, contrasenia, estatus, 
+                //?rol
+                idRol, } = req.body;
+                let querySQL = `UPDATE persona SET
+        nombre='${nombre}',
+        apellidoPaterno='${apellidoPaterno}',
+        apellidoMaterno='${apellidoMaterno}',
+        calle='${calle}',
+        colonia='${colonia}',
+        numero='${numero}',
+        codigo_postal=${codigo_postal},
+        fechaNacimiento='${fechaNacimiento}',
+        genero=${genero},
+        telefono='${telefono}'
+        WHERE idPersona=${idPersona};`;
+                let respuesta = yield execute.query(querySQL);
+                if (!respuesta.validacion) {
+                    yield transaction.rollBackTransaction();
+                    res.send({
+                        code: Types_1.HttpCodes.error,
+                        description: respuesta.descripcion,
+                    });
+                    return;
+                }
+                querySQL = `UPDATE usuarioLogin SET 
+      usuario='${usuario}',
+      contrasenia='${contrasenia}',
+      estatus=${estatus}
+      WHERE idUsuario=${idUsuario};`;
+                respuesta = yield execute.query(querySQL);
+                if (!respuesta.validacion) {
+                    yield transaction.rollBackTransaction();
+                    res.send({
+                        code: Types_1.HttpCodes.error,
+                        description: respuesta.descripcion,
+                    });
+                    return;
+                }
+                yield transaction.commit();
+                res.send({
+                    code: Types_1.HttpCodes.aceptacion,
+                    description: Types_1.descriptions.aceptacion,
+                    data: respuesta.data,
+                });
+            }
+            catch (e) {
+                res.send({
+                    code: Types_1.HttpCodes.error,
+                    description: e.message,
+                    data: null,
+                });
+            }
+        });
     }
 }
 exports.ControllerClientes = ControllerClientes;
--- src/controllers/ControllerClientes.ts
+++ src/controllers/ControllerClientes.ts
@@ -3,13 +3,11 @@ import { TRANSACTIONMYSQL } from "../helpers/StartTransaction";
 import { descriptions, HttpCodes } from "../helpers/Types";
 import { Excecute } from "../services/executeServices";
 
-
 let execute = new Excecute();
-let transaction=new TRANSACTIONMYSQL();
-export class ControllerClientes{
-
-    getClientes = async (req: Request, res: Response) => {
-        try {
+let transaction = new TRANSACTIONMYSQL();
+export class ControllerClientes {
+  getClientes = async (req: Request, res: Response) => {
+    try {
       let querySQL = `SELECT * FROM getClientes;`;
       let respuesta: any = await execute.query(querySQL);
       if (respuesta.validacion) {
@@ -32,33 +30,31 @@ export class ControllerClientes{
       });
     }
   };
-    addCliente = async (req: Request, res: Response) => {
+  addCliente = async (req: Request, res: Response) => {
+    let persona: any = 0;
+    let usuarioData: any = 0;
+    try {
+      await transaction.startTransaction();
 
-      let persona:any=0
-      let usuarioData:any=0
-        try {
-          await transaction.startTransaction();
-
-          let {
-            //?persona
-               nombre,
-               apellidoPaterno,
-               apellidoMaterno,
-               calle,
-               colonia,
-               numero,
-               codigo_postal,
-               fechaNacimiento,
-               genero,
-               telefono,
-              //?usuarioLogin
-                usuario,
-                contrasenia,
-                estatus,
-                //?rol
-                idRol,
-
-              }=req.body;
+      let {
+        //?persona
+        nombre,
+        apellidoPaterno,
+        apellidoMaterno,
+        calle,
+        colonia,
+        numero,
+        codigo_postal,
+        fechaNacimiento,
+        genero,
+        telefono,
+        //?usuarioLogin
+        usuario,
+        contrasenia,
+        estatus,
+        //?rol
+        idRol,
+      } = req.body;
       let querySQL = `INSERT INTO persona ( 
         nombre ,
         apellidoPaterno ,
@@ -82,56 +78,55 @@ export class ControllerClientes{
          '${fechaNacimiento}',
          ${genero},
          '${telefono}');`;
-         
-         let respuesta: any = await execute.query(querySQL);
-         if (!respuesta.validacion) {
-           await transaction.rollBackTransaction();
-        res.send({
-          code: HttpCodes.error,
-          description: respuesta.descripcion,
-        });
-        return
-      } 
-      
-      persona=respuesta.data.insertId;
-      querySQL=`INSERT INTO usuarioLogin(usuario,contrasenia,estatus) VALUES(
-        '${usuario}',
-        '${contrasenia}',
-         ${estatus}
-      );`
-       respuesta = await execute.query(querySQL);
-       usuarioData=respuesta.data.insertId;
-       if (!respuesta.validacion) {
-         await transaction.rollBackTransaction();
-         res.send({
-           code: HttpCodes.error,
-          description: respuesta.descripcion,
-        });
-        return
-      } 
-      
-      querySQL=`INSERT INTO cliente (idPersona,idRol,idUsuario) VALUES(
-         ${persona},
-         ${idRol},
-         ${usuarioData}
-      );`
-       respuesta = await execute.query(querySQL);
 
-       if (!respuesta.validacion) {
+      let respuesta: any = await execute.query(querySQL);
+      if (!respuesta.validacion) {
         await transaction.rollBackTransaction();
         res.send({
           code: HttpCodes.error,
           description: respuesta.descripcion,
         });
-        return
-      } 
+        return;
+      }
+
+      persona = respuesta.data.insertId;
+      querySQL = `INSERT INTO usuarioLogin(usuario,contrasenia,estatus) VALUES(
+        '${usuario}',
+        '${contrasenia}',
+         ${estatus}
+      );`;
+      respuesta = await execute.query(querySQL);
+      usuarioData = respuesta.data.insertId;
+      if (!respuesta.validacion) {
+        await transaction.rollBackTransaction();
+        res.send({
+          code: HttpCodes.error,
+          description: respuesta.descripcion,
+        });
+        return;
+      }
+
+      querySQL = `INSERT INTO cliente (idPersona,idRol,idUsuario) VALUES(
+         ${persona},
+         ${idRol},
+         ${usuarioData}
+      );`;
+      respuesta = await execute.query(querySQL);
+
+      if (!respuesta.validacion) {
+        await transaction.rollBackTransaction();
+        res.send({
+          code: HttpCodes.error,
+          description: respuesta.descripcion,
+        });
+        return;
+      }
       await transaction.commit();
       res.send({
         code: HttpCodes.aceptacion,
         description: descriptions.aceptacion,
         data: respuesta.data,
       });
-
     } catch (e: any) {
       res.send({
         code: HttpCodes.error,
@@ -140,9 +135,83 @@ export class ControllerClientes{
       });
     }
   };
+  updateCliente = async (req: Request, res: Response) => {
+   
+    try {
+      await transaction.startTransaction();
 
+      let {
+        //?cliente
+        idPersona,
+        idUsuario,
+        //?persona
+        nombre,
+        apellidoPaterno,
+        apellidoMaterno,
+        calle,
+        colonia,
+        numero,
+        codigo_postal,
+        fechaNacimiento,
+        genero,
+        telefono,
+        //?usuarioLogin
+        usuario,
+        contrasenia,
+        estatus,
+        //?rol
+        idRol,
+      } = req.body;
+      let querySQL = `UPDATE persona SET
+        nombre='${nombre}',
+        apellidoPaterno='${apellidoPaterno}',
+        apellidoMaterno='${apellidoMaterno}',
+        calle='${calle}',
+        colonia='${colonia}',
+        numero='${numero}',
+        codigo_postal=${codigo_postal},
+        fechaNacimiento='${fechaNacimiento}',
+        genero=${genero},
+        telefono='${telefono}'
+        WHERE idPersona=${idPersona};`;
 
+      let respuesta: any = await execute.query(querySQL);
+      if (!respuesta.validacion) {
+        await transaction.rollBackTransaction();
+        res.send({
+          code: HttpCodes.error,
+          description: respuesta.descripcion,
+        });
+        return;
+      }
 
+      querySQL = `UPDATE usuarioLogin SET 
+      usuario='${usuario}',
+      contrasenia='${contrasenia}',
+      estatus=${estatus}
+      WHERE idUsuario=${idUsuario};`;
+      respuesta = await execute.query(querySQL);
+      if (!respuesta.validacion) {
+        await transaction.rollBackTransaction();
+        res.send({
+          code: HttpCodes.error,
+          description: respuesta.descripcion,
+        });
+        return;
+      }
 
-
+      await transaction.commit();
+      res.send({
+        code: HttpCodes.aceptacion,
+        description: descriptions.aceptacion,
+        data: respuesta.data,
+      });
+    } catch (e: any) {
+      res.send({
+        code: HttpCodes.error,
+        description: e.message,
+        data: null,
+      });
     }
+  };
+}
